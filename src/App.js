42768fc28418ae3f0dd6f693c6e71bcea66c20ca
marioquiroga <mquiroga@dosalcubo.com>
2021-01-26 20:55:41 -0300

    bootstrap & NavBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 
+import NavBar from './components/NavBar';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mario Quiroga App</h1>
-      </header>
-    </div>
+    <NavBar />
   );
 }
 
